c213e6980ceb9d7154baeadb02d1ad42eeca8182
Nrusso26 <nrusso55@hotmail.com>
2021-11-30 11:30:57 -0800

    Update loopy_loops.py

--- loopy_loops.py
+++ loopy_loops.py
@@ -1,5 +1,14 @@
 
 pokemon = ("picachu", "charmander", "bulbasaur")
-    
-for i in range(len(pokemon)):
-    print(pokemon[i])
+picachu, charmander, bulbasaur = pokemon
+pokemon = ("Starter1", "Starter2", "Starter3")
+name = ("N" "A" "T" "H" "A" "N")
+
+
+
+for i in range(2, 11):
+    print(i, end= " ")
+    print()
+
+
+
